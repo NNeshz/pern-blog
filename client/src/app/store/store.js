0d40da2aecd24a70767ab9d2620160d99e386bfa
Nefta Hernandez <neftaligow01@gmail.com>
2023-10-04 20:26:43 -0600

    Usaremos context en lugar de Redux

--- client/src/app/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../features/user/userSlice.js"
-
-export default configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
