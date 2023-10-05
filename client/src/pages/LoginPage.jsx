14cbd98397040cc1d32e7b3bc13ac3619182a88d
Nefta Hernandez <neftaligow01@gmail.com>
2023-10-04 20:28:12 -0600

    Nuevo reinicio

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,46 +1,7 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { signinUser } from "../app/actions/user/userActions";
-
 function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const dispatch = useDispatch();
-  const { errors } = useSelector((state) => state.user);
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    dispatch(signinUser({ email, password }));
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit} className="flex">
-        {errors &&
-          errors.map((err, i) => (
-            <p key={i} className="bg-red-600 text-white px-3 py-2">
-              {err}
-            </p>
-          ))}
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          onChange={(e) => setEmail(e.target.value)}
-          className="bg-zinc-800 text-white"
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
-          className="bg-zinc-800 text-white"
-        />
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  );
+    <div>LoginPage</div>
+  )
 }
 
-export default LoginPage;
+export default LoginPage
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,65 +1,7 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { signupUser } from "../app/actions/user/userActions";
-
-import { Card } from "../components/ui";
-
 function RegisterPage() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const dispatch = useDispatch();
-  const { errors, isAuth } = useSelector((state) => state.user);
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    dispatch(signupUser({ username, email, password }))
-    // console.log(isAuth)
-  };
-
   return (
-    <div>
-      <Card>
-        <form onSubmit={onSubmit} className="">
-          {errors &&
-            errors.map((err, i) => (
-              <p key={i} className="bg-red-600 text-white px-3 py-2">
-                {err}
-              </p>
-            ))}
-          <label htmlFor="username" className="block">
-            Username:
-          </label>
-          <input
-            type="text"
-            id="username"
-            onChange={(e) => setUsername(e.target.value)}
-            className="bg-zinc-800 text-white block"
-          />
-          <label htmlFor="email" className="block">
-            Email:
-          </label>
-          <input
-            type="email"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-zinc-800 text-white block"
-          />
-          <label htmlFor="password" className="block">
-            Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
-            className="bg-zinc-800 text-white block"
-          />
-          <button type="submit">Register</button>
-        </form>
-      </Card>
-    </div>
-  );
+    <div>RegisterPage</div>
+  )
 }
 
-export default RegisterPage;
+export default RegisterPage
