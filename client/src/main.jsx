26b968dbd91aa2f40a7aaab03f86d51de1d27361
Nefta Hernandez <neftaligow01@gmail.com>
2023-10-04 20:28:53 -0600

    Nuevo reinicio

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,15 +5,10 @@ import './index.css'
 
 import { BrowserRouter } from 'react-router-dom'
 
-import { Provider } from 'react-redux'
-import store from './app/store/store.js'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
